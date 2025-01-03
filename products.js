7fc4a579130745d9bedab20aad39c24b356422ae
vidhya-balaji <vidhyabalaji.nina@gmail.com>
2025-01-04 01:12:25 +0530

    collection

---
+++ products.js
@@ -0,0 +1,162 @@
+const productsList = [
+    {
+        id:1,
+        name:"Floral Kurta",
+        src:"image/Collections/K1.jpg",
+        desc:"",
+        price:1000,
+        tags:["new","pink","kurtas","aurelia"]
+    },
+
+    {
+        id:2,
+        name:"Embroidered Kurta",
+        src:"image/Collections/K2.jpg",
+        desc:"",
+        price:1500,
+        tags:["new","blue","kurtas","aurelia"]
+    },
+
+    {
+        id:3,
+        name:"Floral Kurta",
+        src:"image/Collections/K3.jpg",
+        desc:"",
+        price:800,
+        tags:["old","red","kurtas","biba"]
+    },
+
+    {
+        id:4,
+        name:"Plain Kurta",
+        src:"image/Collections/K4.jpg",
+        desc:"",
+        price:650,
+        tags:["old","green","kurtas","aurelia"]
+    },
+    {
+        id:5,
+        name:"Printed Kurta",
+        src:"image/Collections/K5.jpg",
+        desc:"",
+        price:579,
+        tags:["new","white","kurtas","biba"]
+    }
+,
+    {
+        id:6,
+        name:"Straight Kurta",
+        src:"image/Collections/K6.jpg",
+        desc:"",
+        price:579,
+        tags:["old","purple","kurtas","aurelia"]
+    },
+
+    {
+        id:7,
+        name:"White Kurta Set",
+        src:"image/Collections/KS1.jpg",
+        desc:"",
+        price:1200,
+        tags:["new","white","kurtasets","aurelia"]
+    },
+    {
+        id:8,
+        name:"Tradition Kurta Set",
+        src:"image/Collections/KS2.jpg",
+        desc:"",
+        price:1000,
+        tags:["old","red","kurtasets","biba"]
+    },
+    {
+        id:9,
+        name:"Black Elite Kurta",
+        src:"image/Collections/KS3.jpg",
+        desc:"",
+        price:1500,
+        tags:["new","black","kurtasets","biba"]
+    },
+    {
+        id:10,
+        name:"Purple Flower Set",
+        src:"image/Collections/KS4.jpg",
+        desc:"",
+        price:1600,
+        tags:["new","purple","kurtasets","biba"]
+    },
+    {
+        id:11,
+        name:"Elegant Saree",
+        src:"image/Collections/S1.jpg",
+        desc:"",
+        price:2200,
+        tags:["new","purple","sarees","biba"]
+    },
+    {
+        id:12,
+        name:"Green Tissue Saree",
+        src:"image/Collections/S3.jpg",
+        desc:"",
+        price:1900,
+        tags:["new","green","sarees","aurelia"]
+    },
+
+    {
+        id:13,
+        name:"Puff Sleeve Dress",
+        src:"image/Collections/D1.jpg",
+        desc:"",
+        price:800,
+        tags:["new","green","dresses","ck"]
+    },
+    {
+        id:14,
+        name:"Black Printed Dress",
+        src:"image/Collections/D2.jpg",
+        desc:"",
+        price:500,
+        tags:["new","black","dresses","ck"]
+    },
+    {
+        id:15,
+        name:" Floral Printed Dress",
+        src:"image/Collections/D3.jpg",
+        desc:"",
+        price:220,
+        tags:["new","white","dresses","ck"]
+    },
+
+    {
+        id:16,
+        name:"Embroidered Kurta",
+        src:"image/Collections/E1.jpg",
+        desc:"",
+        price:2700,
+        tags:["new","green","ethnic","fabIndia"]
+    },
+    {
+        id:17,
+        name:"Black Printed Kurta",
+        src:"image/Collections/E4.jpg",
+        desc:"",
+        price:2500,
+        tags:["new","black","ethnic","fabIndia"]
+    },    
+    {
+        id:18,
+        name:"Purple Gold Kurta",
+        src:"image/Collections/E5.jpg",
+        desc:"",
+        price:2200,
+        tags:["new","purple","ethnic","fabIndia"]
+    },
+    {
+        id:19,
+        name:"Leather Handbags",
+        src:"image/Collections/B1.jpg",
+        desc:"",
+        price:1000,
+        tags:["new","black","bags","ck"]
+    },
+]
+
